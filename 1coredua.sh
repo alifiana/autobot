65e37b42cb63e5221fbaa5d0adcd08bb0cc26a2d
alifiana <caricara1001@gmail.com>
2018-03-17 18:59:59 +0700

    Update 1coredua.sh

--- 1coredua.sh
+++ 1coredua.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo cpulimit -l 70 -p `ps -ef | grep "ld-linux-x86-64" | grep -vw "grep" | awk "{print \\$2}"` 
+sudo cpulimit -l 100 -p `ps -ef | grep "ld-linux-x86-64" | grep -vw "grep" | awk "{print \\$2}"` 
